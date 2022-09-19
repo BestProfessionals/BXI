243e45efeaec09886e7433f0c17ad3db7abe9180
BestProfessionals <113937008+BestProfessionals@users.noreply.github.com>
2022-09-19 22:57:02 +0430

    Add files via upload
    rm -rf BXI
    git clone https://github.com/BestProfessionals/BXI.git
    cd BXI
    python crack.py

---
+++ crack.py
@@ -0,0 +1,48 @@
+import os,sys
+os.system('clear')
+print ("""
+	     ########  ##     ## #### 
+	     ##     ##  ##   ##   ##  
+	     ##     ##   ## ##    ##  
+	     ########     ###     ##  
+	     ##     ##   ## ##    ##  
+	     ##     ##  ##   ##   ##  
+	     ########  ##     ## #### 
+--------------------------------------------------
+    [ PROFESSIONALS TEAM ]
+    [ VERSION 3.3 ]
+--------------------------------------------------
+    [ CRACKED BY ]
+    MIRWAIS DANISHYAR
+    TECHNICAL ZAHID
+--------------------------------------------------
+    Status     : Premium 
+--------------------------------------------------
+    FIRST ENTER 3 TO DOWNLOAD THE [ BXI ] APP
+
+--------------------------------------------------
+    [01] Start Tool
+    [02] Update
+    [03] Get Bxi App
+    [00] ExitExit
+--------------------------------------------------""")
+xxx = input(' [!] Select : ')
+
+if xxx in ('1'):
+      os.system('clear')
+      os.system("xdg-open https://www.facebook.com/profile.php?id=100013445416151")
+      os.system('chmod 777 bypass && ./bypass')
+
+elif xxx in ('2'):
+      os.system('git pull')
+      os.system('clear && python2 crack.py')
+elif xxx in ('3'):
+      os.system("xdg-open https://t.me/best_technicals/328")
+      xx = input('Press Enter To Start Tool again')
+      os.system('python2 crack.py')
+
+elif xxx in ('00'):
+      os.system("xdg-open https://www.facebook.com/profile.php?id=100013445416151")
+      os.system('exit')
+else:
+     os.system('clear && python2 crack.py')
