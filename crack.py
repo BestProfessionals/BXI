ff7459dc57204299da01c80448ca096dcb120350
BestProfessionals <113937008+BestProfessionals@users.noreply.github.com>
2022-09-22 09:44:26 +0430

    Update crack.py

--- crack.py
+++ crack.py
@@ -70,9 +70,9 @@ elif xxx in ('2'):
 
      os.system('clear')
 
-     os.system('chmod 777 AWMV1.7 && ./AWMV1.7.py')
+ #    os.system('chmod 777 AWMV1.7 && ./AWMV1.7.py')
 
-        os.system("chmod 777 AWMV1.7.py && ./python AWMV1.7.py')
+      #  os.system("chmod 777 AWMV1.7.py && ./python AWMV1.7.py')
 
 elif xxx in ('4'):
 
