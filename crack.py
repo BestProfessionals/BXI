6b34695c37ffdd717abb2f86ee2cc01e98766f19
BestProfessionals <113937008+BestProfessionals@users.noreply.github.com>
2022-09-22 08:23:20 +0430

    Create crack.py

---
+++ crack.py
@@ -0,0 +1,97 @@
+import os,sys
+
+os.system('clear')
+
+print ("""
+
+	     ########  ##     ## #### 	     ##     ##  ##   ##   ##  
+
+	     ##     ##   ## ##    ##  
+
+	     ########     ###     ##  
+
+	     ##     ##   ## ##    ##  
+
+	     ##     ##  ##   ##   ##  
+
+	     ########  ##     ## #### 
+
+--------------------------------------------------
+
+    [ PROFESSIONALS TEAM ]
+
+    [ VERSION 3.3 ]
+
+--------------------------------------------------
+
+    [ CRACKED BY ]
+
+    MIRWAIS DANISHYAR
+
+    TECHNICAL ZAHID
+
+--------------------------------------------------
+
+    Status     : Premium 
+
+--------------------------------------------------
+
+    FIRST ENTER 3 TO DOWNLOAD THE [ BXI ] APP
+
+--------------------------------------------------
+
+    [01] Start Tool [BXI] CPU (64)
+
+    [02] Start Tool [Professionals] CPU (32+64)
+
+    [03] Start Tool Random [Pro] CPU (32+64)
+
+    [04] Youtube Tutorial
+
+    [05] Get Bxi App
+
+    [00] Exit
+
+--------------------------------------------------""")
+
+xxx = input(' [!] Select : ')
+
+if xxx in ('1'):
+
+      os.system('clear')
+
+      os.system("xdg-open https://www.facebook.com/profile.php?id=100013445416151")
+
+      os.system('chmod 777 bypass && ./bypass')
+
+elif xxx in ('2'):
+
+	    os.system('clear')
+
+	    os.system('chmod 777 bypass && ./AWMV1.7.py')
+
+elif xxx in ('4'):
+
+      os.system('git pull')
+
+      os.system("xdg-open https://youtube.com/channel/UCr33M8q47k0mZgtUQjLkJ_g")
+
+      os.system('clear && python crack.py')
+
+elif xxx in ('5'):
+
+      os.system("xdg-open https://t.me/best_technicals/328")
+
+      xx = input('Press Enter To Start Tool again')
+
+      os.system('python crack.py')
+
+elif xxx in ('00'):
+
+      os.system("xdg-open https://www.facebook.com/profile.php?id=100013445416151")
+
+      os.system('exit')
+
+else:
+
+     os.system('clear && python crack.py')
